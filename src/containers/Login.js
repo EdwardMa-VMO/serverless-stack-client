e1670d173d0136931d5d634767df2d900f2badb9
Edward Ma <ma.edward@hotmail.com>
2019-11-28 14:26:47 -0400

    Signup with AWS Cognito

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,21 +3,24 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import LoaderButton from "../components/LoaderButton";
+import { useFormFields } from "../libs/hooksLib";
 
 export default function Login(props) {
     const [isLoading, setIsLoading] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [fields, handleFieldChange] = useFormFields({
+      email: "",
+      password: ""
+    });
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
 
     async function handleSubmit(event) {
         event.preventDefault();
         setIsLoading(true);
     try {
-        await Auth.signIn(email, password);
+        await Auth.signIn(fields.email, fields.password);
         props.userHasAuthenticated(true);
         props.history.push("/");
     } catch (e) {
@@ -34,15 +37,15 @@ export default function Login(props) {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </FormGroup>

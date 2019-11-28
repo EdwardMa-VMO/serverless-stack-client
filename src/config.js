6109ce031eb8f7b49b39637639ab9f10de871367
Edward Ma <ma.edward@hotmail.com>
2019-11-28 12:47:26 -0400

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "mahokkit-notes-app-uploads1"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://sg8lr6s16h.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_0yXRKfHJT",
+      APP_CLIENT_ID: "mfno6ipdt0vcpvhgecru5g2nf",
+      IDENTITY_POOL_ID: "us-east-1:ffc5c343-2a17-46fa-a78c-dbd890210ddf"
+    }
+  };
